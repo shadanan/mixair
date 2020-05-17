d34ef5ff96496a7f9cc0ce98ad83b01483fe8d1c
Shad Sharma <shadanan@gmail.com>
2020-05-17 15:04:15 -0700

    Make layout of channels responsive. Fixes ##16.

--- src/Mixer.tsx
+++ src/Mixer.tsx
@@ -1,4 +1,4 @@
-import Box from "@material-ui/core/Box";
+import Grid from "@material-ui/core/Grid";
 import React, { useEffect } from "react";
 import Channel from "./Channel";
 import { XAir } from "./XAir";
@@ -17,39 +17,44 @@ export default function Mixer({ mixer }: MixerProps) {
   }, [xair]);
 
   return (
-    <Box>
-      <Channel
-        xair={xair}
-        channelName={"LR"}
-        nameAddress={"/lr/config/name"}
-        muteAddress={"/lr/mix/on"}
-        soloAddress={"/-stat/solosw/50"}
-        faderAddress={"/lr/mix/fader"}
-      />
-      <Channel
-        xair={xair}
-        channelName={"Aux"}
-        nameAddress={"/rtn/aux/config/name"}
-        muteAddress={"/rtn/aux/mix/on"}
-        soloAddress={"/-stat/solosw/17"}
-        faderAddress={"/rtn/aux/mix/fader"}
-        meterId={17}
-      />
+    <Grid container>
+      <Grid item xs={12} md={6} lg={4} xl={3}>
+        <Channel
+          xair={xair}
+          channelName={"LR"}
+          nameAddress={"/lr/config/name"}
+          muteAddress={"/lr/mix/on"}
+          soloAddress={"/-stat/solosw/50"}
+          faderAddress={"/lr/mix/fader"}
+        />
+      </Grid>
+      <Grid item xs={12} md={6} lg={4} xl={3}>
+        <Channel
+          xair={xair}
+          channelName={"Aux"}
+          nameAddress={"/rtn/aux/config/name"}
+          muteAddress={"/rtn/aux/mix/on"}
+          soloAddress={"/-stat/solosw/17"}
+          faderAddress={"/rtn/aux/mix/fader"}
+          meterId={17}
+        />
+      </Grid>
       {Array.from({ length: 16 }, (_, i) => {
         const channelLabel = String(i + 1).padStart(2, "0");
         return (
-          <Channel
-            key={channelLabel}
-            xair={xair}
-            channelName={channelLabel}
-            nameAddress={`/ch/${channelLabel}/config/name`}
-            muteAddress={`/ch/${channelLabel}/mix/on`}
-            soloAddress={`/-stat/solosw/${channelLabel}`}
-            faderAddress={`/ch/${channelLabel}/mix/fader`}
-            meterId={i}
-          />
+          <Grid item xs={12} md={6} lg={4} xl={3} key={channelLabel}>
+            <Channel
+              xair={xair}
+              channelName={channelLabel}
+              nameAddress={`/ch/${channelLabel}/config/name`}
+              muteAddress={`/ch/${channelLabel}/mix/on`}
+              soloAddress={`/-stat/solosw/${channelLabel}`}
+              faderAddress={`/ch/${channelLabel}/mix/fader`}
+              meterId={i}
+            />
+          </Grid>
         );
       })}
-    </Box>
+    </Grid>
   );
 }
--- src/MixerSelect.tsx
+++ src/MixerSelect.tsx
@@ -1,13 +1,11 @@
-import {
-  Container,
-  List,
-  ListItem,
-  ListItemIcon,
-  ListItemText,
-  ListSubheader,
-  makeStyles,
-  Paper,
-} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core";
+import Container from "@material-ui/core/Container";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import ListSubheader from "@material-ui/core/ListSubheader";
+import Paper from "@material-ui/core/Paper";
 import GraphicEqIcon from "@material-ui/icons/GraphicEq";
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
